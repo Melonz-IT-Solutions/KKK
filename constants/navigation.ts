7a8330b6e38ef22b037942e028c1b389731b9da6
Khalid Usman <kusman758@gmail.com>
2026-05-16 21:49:05 +0800

    feat: sidebar

---
+++ constants/navigation.ts
@@ -0,0 +1,23 @@
+import {
+  Home,
+  Users,
+  Settings,
+} from "lucide-react"
+
+export const sidebarLinks = [
+  {
+    title: "Dashboard",
+    href: "/",
+    icon: Home,
+  },
+  {
+    title: "Users",
+    href: "/users",
+    icon: Users,
+  },
+  {
+    title: "Settings",
+    href: "/settings",
+    icon: Settings,
+  },
+]

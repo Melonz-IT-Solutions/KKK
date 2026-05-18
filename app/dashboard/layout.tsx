ed07f821590c08e73ad6dbc374c29b1af890eaf0
Khalid Usman <kusman758@gmail.com>
2026-05-19 00:01:32 +0800

    chore: move cards to page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import { AppSidebar } from '@/components/layout/sidebar/app-sidebar'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import SidebarHeader from '@/components/layout/sidebar/header'
-import DashboardCard from '@/components/cards/dashboard-card'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,13 +8,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <AppSidebar />
       <SidebarInset>
         <SidebarHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-0">
-              <DashboardCard />
-            </div>
-          </div>
-        </div>
+        {children}
       </SidebarInset>
     </SidebarProvider>
   )

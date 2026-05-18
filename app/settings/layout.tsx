d9b5a42cfb6e46a9c140355ae6394633fde08624
Khalid Usman <kusman758@gmail.com>
2026-05-19 00:07:15 +0800

    chore: move cards to page

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -1,28 +1,15 @@
 import { AppSidebar } from '@/components/layout/sidebar/app-sidebar'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb'
+import SidebarHeader from '@/components/layout/sidebar/header'
 import { Separator } from '@/components/ui/separator'
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 
 export default function MembersLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min" />
-        </div>
+        <SidebarHeader />
+        {children}
       </SidebarInset>
     </SidebarProvider>
   )
